5a84d49f2cbb95a6c5933c426ff2bc9f4a116ebb
andre84o <andre.torabpour@gmail.com>
2025-06-15 18:28:13 +0200

    add link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
 import RandomUserComponent from "@/components/RandomUser";
 
 export default function UserPage() {
-  return <RandomUserComponent />;
+  return (
+    <>
+      <a
+        href="https://csslibraries-app1.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          textDecoration: 'underline',
+          display: 'block',
+          marginBottom: '20px',
+          color: '#0070f3',
+          textAlign: 'center',
+          fontWeight: 'bold',
+        }}
+      >
+      Todo App
+      </a>
+
+      <RandomUserComponent />
+    </>
+  );
 }
